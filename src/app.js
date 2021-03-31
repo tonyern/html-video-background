964f312e44ab2416f9407de92d742c791850e063
Tony <tony.d.nguyen@protonmail.com>
2021-03-31 09:56:09 -0500

    Implemented layout of the app.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+var video = document.querySelector("vac-video");
+var button = document.querySelector("videoBtn");
+var videoPlayPause = function () {
+    /*if (video.paused) {
+        video.play();
+        button.innerHTML = "Pause";
+    } else {
+        video.pause();
+        button.innerHTML = "Play";
+    }*/
+    //video.pause();
+    button.innerHTML = "Play";
+};
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,15 @@
-console.log("Hello World");
+let video: HTMLVideoElement = document.querySelector("vac-video");
+let button: HTMLButtonElement = document.querySelector("videoBtn");
+
+const videoPlayPause = (): void => {
+    /*if (video.paused) {
+        video.play();
+        button.innerHTML = "Pause";
+    } else {
+        video.pause();
+        button.innerHTML = "Play";
+    }*/
+
+    //video.pause();
+    button.innerHTML = "Play";
+}
